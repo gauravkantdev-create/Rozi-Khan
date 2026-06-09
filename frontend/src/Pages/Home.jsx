93023deeb5dbecb8909385f8c18bc2ab48814603
Gaurav Kant <gauravkant.dev@gmiail.com>
2026-06-10 00:17:00 +0530

    logo.png

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import HomeCollections from "../Components/home/HomeCollections";
 import { Container, Eyebrow, PageShell, PrimaryLink, SecondaryLink, SectionHeading, StatCard, surfaceClass } from "../Components/layout/PageShell";
 import useAuthStatus from "../hooks/useAuthStatus";
