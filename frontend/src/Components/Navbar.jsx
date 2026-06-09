33724aad9a59cd295ccaa1eccff9c5753e651445
Gaurav Kant <gauravkant.dev@gmiail.com>
2026-06-10 00:24:17 +0530

    Fix logo import casing for Vercel

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import useAuth from "../hooks/useAuth";
 import useCart from "../hooks/useCart";
 import useThemeMode from "../hooks/useThemeMode";
